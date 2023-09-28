1ccb79d179fa7a3b40a798b3113ad29734b7e6ce
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-09-28 16:41:41 +0600

    Character selection screen added

--- lib/screens/character_selection_screen.dart
+++ lib/screens/character_selection_screen.dart
@@ -1,5 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:eclipse/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
 class CharacterSelectionScreen extends StatefulWidget {
   final String userName;
@@ -14,7 +16,8 @@ class CharacterSelectionScreen extends StatefulWidget {
 }
 
 class _CharacterSelectionScreenState extends State<CharacterSelectionScreen> {
-  int selectedImageIndex = -1; // Initialize with no selection
+  int selectedImageIndex = 0; // Initialize with no selection
+  CarouselController controller = CarouselController();
 
   final List<String> imagePaths = [
     'assets/image1.png',
@@ -25,6 +28,8 @@ class _CharacterSelectionScreenState extends State<CharacterSelectionScreen> {
 
   @override
   Widget build(BuildContext context) {
+    int pageLength = imagePaths.length;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Character Selection'),
@@ -41,38 +46,68 @@ class _CharacterSelectionScreenState extends State<CharacterSelectionScreen> {
               ),
             ),
             const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(4, (index) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedImageIndex = index;
-                    });
-                  },
-                  child: Container(
-                    width: 150.0,
-                    height: 150.0,
-                    margin: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: selectedImageIndex == index
-                            ? Colors.blue
-                            : Colors.transparent,
-                        width: 2.0,
+            CarouselSlider(
+              carouselController: controller,
+              items: imagePaths.map((imagePath) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: 250.0,
+                      margin: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: selectedImageIndex ==
+                                  imagePaths.indexOf(imagePath)
+                              ? Colors.blue
+                              : Colors.transparent,
+                          width: 2.0,
+                        ),
                       ),
-                    ),
-                    child: Image.asset(
-                      imagePaths[index],
-                      width: 150.0,
-                      height: 150.0,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+                      child: Image.asset(
+                        imagePath,
+                        width: 240.0,
+                        height: 240.0,
+                        fit: BoxFit.scaleDown,
+                      ),
+                    );
+                  },
                 );
-              }),
+              }).toList(),
+              options: CarouselOptions(
+                height: 250.0,
+                enlargeCenterPage: true,
+                enableInfiniteScroll: false,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    selectedImageIndex = index;
+                  });
+                },
+                // Customize the appearance of the dots
+                viewportFraction: 1,
+                aspectRatio: 2.0,
+                autoPlay: false,
+                enlargeStrategy: CenterPageEnlargeStrategy.scale,
+                scrollDirection: Axis.horizontal,
+              ),
             ),
             const SizedBox(height: 20.0),
+            // DotsIndicator widget to display dots
+            DotsIndicator(
+              dotsCount: pageLength,
+              position: selectedImageIndex,
+              decorator: DotsDecorator(
+                size: const Size.square(9.0),
+                activeSize: const Size(18.0, 9.0),
+                activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                color:
+                    Colors.grey, // Set the color of inactive dots (e.g., grey)
+                activeColor: Colors
+                    .orange, // Set the color of the selected (active) dot to orange
+              ),
+            ),
+            const SizedBox(height: 40.0),
             ElevatedButton(
               onPressed: () {
                 if (selectedImageIndex >= 0 &&
