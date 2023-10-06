a771d308ac34afda2ae589bfdffbbbe1ae6877fd
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-10-06 09:49:03 +0600

    Final commit

--- lib/screens/pages/quiz_game.dart
+++ lib/screens/pages/quiz_game.dart
@@ -29,7 +29,7 @@ class _QuizGameState extends State<QuizGame> {
 
     // Shuffle the list of questions
     final random = Random();
-    shuffledQuestions = List.from(eclipseQuestions); // Make a copy
+    shuffledQuestions = List.from(eclipseQuestions);
     shuffledQuestions.shuffle(random);
 
     // Ensure only 5 questions are displayed
@@ -99,7 +99,7 @@ class _QuizGameState extends State<QuizGame> {
       correctAnswers = 0;
       remainingTime = 180;
       final random = Random();
-      shuffledQuestions.shuffle(random); // Shuffle questions again
+      shuffledQuestions.shuffle(random);
     });
     startTimer();
   }
@@ -107,79 +107,93 @@ class _QuizGameState extends State<QuizGame> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // title: const Text('Quiz App'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ValueListenableBuilder<int>(
-              valueListenable: timerNotifier,
-              builder: (context, time, child) {
-                final minutes = (time / 60).floor();
-                final seconds = time % 60;
-                return Row(
-                  children: [
-                    const Icon(Icons.access_time),
-                    Text('$minutes:$seconds'),
-                  ],
-                );
-              },
+      body: Center(
+        // Wrap the Stack with Center
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Lottie.asset(
+                'assets/storybg.json',
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          // Lottie Animation as Background
-          Positioned.fill(
-            child: Lottie.asset(
-              'assets/storybg.json', // Replace with the actual path to your Lottie animation JSON file
-              fit: BoxFit.cover,
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    'Question ${currentQuestionIndex + 1}/$numberOfQuestionsToShow',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                  const SizedBox(height: 10),
-                  Center(
-                    child: Text(
-                      shuffledQuestions[currentQuestionIndex].text,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(fontSize: 24, color: Colors.white),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  ...shuffledQuestions[currentQuestionIndex]
-                      .options
-                      .asMap()
-                      .entries
-                      .map(
-                    (entry) {
-                      final index = entry.key;
-                      final option = entry.value;
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 10.0),
-                        child: ElevatedButton(
-                          onPressed: () => checkAnswer(index),
-                          child: Text(option),
-                        ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: ValueListenableBuilder<int>(
+                    valueListenable: timerNotifier,
+                    builder: (context, time, child) {
+                      final minutes = (time / 60).floor();
+                      final seconds = time % 60;
+                      return Row(
+                        mainAxisAlignment:
+                            MainAxisAlignment.center, // Center the clock
+                        children: [
+                          const Icon(
+                            Icons.access_time,
+                            color: Colors.white, // Set the clock icon color
+                          ),
+                          const SizedBox(
+                              width:
+                                  8), // Add some space between icon and timer
+                          Text(
+                            '$minutes:$seconds',
+                            style: const TextStyle(
+                                fontSize: 24, color: Colors.white),
+                          ),
+                        ],
                       );
                     },
                   ),
-                ],
-              ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Question ${currentQuestionIndex + 1}/$numberOfQuestionsToShow',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontSize: 18, color: Colors.white),
+                        ),
+                        const SizedBox(height: 10),
+                        Center(
+                          child: Text(
+                            shuffledQuestions[currentQuestionIndex].text,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                fontSize: 24, color: Colors.white),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        ...shuffledQuestions[currentQuestionIndex]
+                            .options
+                            .asMap()
+                            .entries
+                            .map(
+                          (entry) {
+                            final index = entry.key;
+                            final option = entry.value;
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 10.0),
+                              child: ElevatedButton(
+                                onPressed: () => checkAnswer(index),
+                                child: Text(option),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
